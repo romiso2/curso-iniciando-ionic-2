1e13b34a4276d02fd9239dccf748f28b93152d75
Robson M. de Souza <romiso2@gmail.com>
2017-01-07 15:35:31 -0200

    capitulo03-aula04 [v2]

--- app/app.ts
+++ app/app.ts
@@ -5,17 +5,26 @@ import {HomePage} from './pages/home/home';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
 export class MyApp {
+  pages: Array<{Component: any, title: String, icon: String}>;
   rootPage: any = HomePage;
 
   constructor(platform: Platform) {
+
+    this.pages = [
+      {Component: HomePage, title:'Home', icon:'home'}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+  openPage(page: any) : void {
+    this.rootPage = page.Component;
   }
 }
 
